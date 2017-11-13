c8e6c2878bf0dc92133bb1b907c2c822b800a7f0
Daan Rosbergen <d.rosbergen@gmail.com>
2017-11-14 00:26:46 +0100

    Add login form

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -3,7 +3,14 @@ import React, { Component } from 'react';
 class Login extends Component {
   render() {
     return (
+      <div>
         <h1>Login</h1>
+        <form>
+          <input type="email" name="email" placeholder="E-mailadres" required />
+          <input type="password" name="password" placeholder="Wachtwoord" required />
+          <button type="submit">Login</button>
+        </form>
+      </div>
     );
   }
 }
