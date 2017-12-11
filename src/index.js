57d513e1fd980de969170d79607345c5807d6ea0
Daan Rosbergen <d.rosbergen@gmail.com>
2017-12-11 20:48:10 +0100

    Add redux, setup store and add root component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import routes from './routes';
-import registerServiceWorker from './registerServiceWorker';
-import 'normalize.css';
-import 'flexboxgrid';
-import 'font-awesome/css/font-awesome.css';
-import '@blueprintjs/core/dist/blueprint.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import app from "./reducers/app";
+import routes from "./routes";
+import registerServiceWorker from "./registerServiceWorker";
+import "normalize.css";
+import "flexboxgrid";
+import "font-awesome/css/font-awesome.css";
+import "@blueprintjs/core/dist/blueprint.css";
 
-ReactDOM.render(<Router children={routes} />, document.getElementById('root'));
+const store = createStore(app);
+
+const Root = ({ store }) => (
+  <Provider store={store}>
+    <Router children={routes} />
+  </Provider>
+);
+
+ReactDOM.render(<Root store={store} />, document.getElementById("root"));
 registerServiceWorker();
---
+++ src/reducers/app.js
@@ -0,0 +1,10 @@
+const initialState = {
+  loggedIn: false
+};
+
+export default function app(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
